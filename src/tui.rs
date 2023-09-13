8e6eed735db41b41da4a5f3d8824413662c9024e
Mike Mehl <michaelalanmehl@gmail.com>
2023-09-13 00:16:06 -0400

    Tui stuff....looking pretty messy.

--- src/tui.rs
+++ src/tui.rs
@@ -1,3 +1,4 @@
+use crate::{db, feed, podcast};
 use crossterm::{event, execute, terminal};
 use ratatui::{prelude::*, widgets};
 use std::io;
@@ -10,24 +11,50 @@ pub fn start() -> Result<(), io::Error> {
     execute!(stdout, terminal::EnterAlternateScreen)?;
     let backend = CrosstermBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
-    terminal.draw(|f| {
-        let size = f.size();
-        let block = widgets::Block::default()
-            .title("Block")
-            .borders(widgets::Borders::ALL)
-            .border_type(widgets::BorderType::Rounded);
-        f.render_widget(block, size);
-    })?;
 
-    thread::spawn(|| loop {
-        let _ = event::read();
-    });
-
-    thread::sleep(Duration::from_secs(5));
+    loop {
+        terminal.draw(main_widget)?;
+        if event::poll(Duration::from_millis(100)).unwrap() {
+            if let event::Event::Key(key) = event::read().unwrap() {
+                if let event::KeyCode::Char('q') = key.code {
+                    break;
+                }
+            }
+        }
+    }
 
     terminal::disable_raw_mode()?;
     execute!(terminal.backend_mut(), terminal::LeaveAlternateScreen)?;
     terminal.show_cursor()?;
 
     Ok(())
+}
+
+fn main_widget(f: &mut Frame<CrosstermBackend<io::Stdout>>) {
+    let size = f.size();
+    let layout = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints([Constraint::Max(1), Constraint::Min(0)].as_ref())
+        .split(size);
+    let title = widgets::Block::default()
+        .title("dipper")
+        .title_alignment(Alignment::Center)
+        .title_style(Style::default().yellow())
+        .borders(widgets::Borders::TOP)
+        .border_type(widgets::BorderType::Double);
+    f.render_widget(title, layout[0]);
+
+    let conn = db::init_db(&"test.db".to_string()).unwrap();
+    let pods = db::fetch_all_podcasts(&conn).unwrap();
+    let mut items = Vec::new();
+    for pod in pods {
+        items.push(widgets::ListItem::new(pod.title));
+    }
+    let pods = widgets::List::new(items).block(
+        widgets::Block::default()
+            .borders(widgets::Borders::TOP)
+            .title("Podcasts"),
+    );
+
+    f.render_widget(pods, layout[1]);
 }
